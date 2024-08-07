c26a0d67481b511ae70bbe9c179f46a0a8630cc0
Jasper Deasey <jasperdeasey@Jaspers-MacBook-Air.local>
2024-08-07 12:28:33 -0700

    updated for heroku upload

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
-from api.routes import configure_routes
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+from backend.routes import configure_routes
+
+load_dotenv()  # Load environment variables from .env file
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='../frontend/build')
     CORS(app)  # Enable CORS for all routes and origins
+
+    # Use DATABASE_URL from environment variables, with a default fallback
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db = SQLAlchemy(app)
+    
     configure_routes(app)
+
+    @app.route('/')
+    def serve():
+        return send_from_directory(app.static_folder, 'index.html')
+
+    @app.route('/<path:path>')
+    def serve_static(path):
+        return send_from_directory(app.static_folder, path)
+
     return app
 
 if __name__ == "__main__":
---
+++ backend/routes.py
@@ -0,0 +1,19 @@
+import json
+from flask import request, jsonify
+import api.analysis as analysis
+
+def configure_routes(app):
+
+    @app.route('/submit-data', methods=['POST'])
+    def submit_data():
+        data = request.get_json()  # Extract JSON data from the request
+        print(data)
+        processed_data = process_data(data)  # Process the data
+        return jsonify(processed_data)  # Return the processed data
+
+    def process_data(data):
+        # Example data processing: append " processed" to each value
+        if data:
+            data = analysis.process_data(data)
+            return data
+        return {"error": "No data received"}
