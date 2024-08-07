f32b8c6fd443fc0a4daaea3c1653980defee8b32
Jasper Deasey <jasperdeasey@Jaspers-MacBook-Air.local>
2024-08-07 13:45:18 -0700

    moved files

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -5,7 +5,7 @@ def create_app():
     app = Flask(__name__)
     CORS(app)  # Enable Cross-Origin Resource Sharing (CORS) if needed
 
-    from ..routes import main_bp
+    from .routes import main_bp
     app.register_blueprint(main_bp)
 
     return app
