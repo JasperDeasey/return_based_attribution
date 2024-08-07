96fe002f1b9e56940f8a40e76c054750d7bfbe06
Jasper Deasey <jasperdeasey@Jaspers-MacBook-Air.local>
2024-08-07 15:23:46 -0700

    Update configuration for Heroku deployment

--- backend/app.py
+++ backend/app.py
@@ -24,13 +24,13 @@ def create_app():
     
     configure_routes(app)
 
-    @app.route('/')
-    def serve():
-        return send_from_directory(app.static_folder, 'index.html')
-
+    @app.route('/', defaults={'path': ''})
     @app.route('/<path:path>')
-    def serve_static(path):
-        return send_from_directory(app.static_folder, path)
+    def serve(path):
+        if path != "" and os.path.exists(app.static_folder + '/' + path):
+            return send_from_directory(app.static_folder, path)
+        else:
+            return send_from_directory(app.static_folder, 'index.html')
 
     return app
 
