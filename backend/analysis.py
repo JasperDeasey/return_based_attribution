24cd0d3b050e2640a113ef61ba0242417bde8010
Jasper Deasey <jasperdeasey@Jaspers-MacBook-Air.local>
2024-08-07 19:15:57 -0700

    added logger

--- backend/analysis.py
+++ backend/analysis.py
@@ -15,7 +15,7 @@ import logging
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
-
+logger = logging.getLogger(__name__)
 
 
 load_dotenv()  # Load environment variables from .env file
