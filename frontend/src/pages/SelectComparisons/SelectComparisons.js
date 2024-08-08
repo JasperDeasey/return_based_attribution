3a6143eb360795ce5956ffa7e359fb3be857f6b6
Jasper Deasey <jasperdeasey@Jaspers-MacBook-Air.local>
2024-08-07 18:44:08 -0700

    LIVIN ON A PRAYER

--- frontend/src/pages/SelectComparisons/SelectComparisons.js
+++ frontend/src/pages/SelectComparisons/SelectComparisons.js
@@ -48,7 +48,7 @@ const SelectComparisons = () => {
     setSnackbarMessage("This may take a few minutes...");
     setSnackbarOpen(true);
 
-    const url = 'https://return-attribution-c87301303521.herokuapp.com/submit-data';
+    const url = '/submit-data';
 
     fetch(url, {
       method: 'POST',
